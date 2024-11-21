b72b1bf430fe25f2f21666a1d49377f6aa8b466b
Ritesh Sharma <riteshrsharma71@gmail.com>
2024-11-22 01:17:39 +0530

    added health check route and controller and few files

---
+++ src/controllers/healthCheck.controller.js
@@ -0,0 +1,17 @@
+import { ApiError } from "../utils/ApiError"
+import { ApiResponse } from "../utils/ApiResponse"
+import { asyncHandler } from "../utils/asyncHandler"
+
+const healthcheck = asyncHandler(async (req, res) => {
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200 , {} , "Ok")
+    )
+});
+
+export {
+    healthcheck
+    }
+    
